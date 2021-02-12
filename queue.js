d7eb5d4f70bc66bb1372831fff0cad266c3bb5b6
Brian Shapiro <briandavidshapiro@gmail.com>
2021-02-12 17:28:02 -0500

    completed everything before further study

--- queue.js
+++ queue.js
@@ -1,46 +1,57 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
 
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+	/** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+	enqueue(val) {
+		const newNode = new Node(val);
+		this.last ? (this.last.next = newNode) : (this.first = newNode);
+		this.last = newNode;
+		this.size++;
+	}
 
-  }
+	/** dequeue(): remove the node from the start of the queue
+	 * and return its value. Should throw an error if the queue is empty. */
 
-  /** dequeue(): remove the node from the start of the queue
-   * and return its value. Should throw an error if the queue is empty. */
+	dequeue() {
+		if (this.size === 0) {
+			throw new Error("Error: The queue is empty");
+		}
+		const oldFirst = this.first;
+		const newFirst = this.first.next;
+		this.first.next = null;
+		this.first = newFirst;
+		this.size--;
+		return oldFirst.val;
+	}
 
-  dequeue() {
+	/** peek(): return the value of the first node in the queue. */
 
-  }
+	peek() {
+		return this.first.val;
+	}
 
-  /** peek(): return the value of the first node in the queue. */
+	/** isEmpty(): return true if the queue is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the queue is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {
+		return this.size === 0;
+	}
 }
 
 module.exports = Queue;
--- stack.js
+++ stack.js
@@ -1,46 +1,57 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {
+		const newNode = new Node(val);
+		this.first ? (newNode.next = this.first) : (this.last = newNode);
+		this.first = newNode;
+		this.size++;
+	}
 
-  }
+	/** pop(): remove the node from the top of the stack
+	 * and return its value. Should throw an error if the stack is empty. */
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+	pop() {
+		if (this.size === 0) {
+			throw new Error("Error: The stack is empty");
+		}
+		const oldFirst = this.first;
+		const newFirst = this.first.next;
+		this.first.next = null;
+		this.first = newFirst;
+		this.size--;
+		return oldFirst.val;
+	}
 
-  pop() {
+	/** peek(): return the value of the first node in the stack. */
 
-  }
+	peek() {
+		return this.first.val;
+	}
 
-  /** peek(): return the value of the first node in the stack. */
+	/** isEmpty(): return true if the stack is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {
+		return this.size === 0;
+	}
 }
 
 module.exports = Stack;
